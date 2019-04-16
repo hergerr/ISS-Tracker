cf61b2c92bc447f75a20481b5d6cf3854e045650
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-04-16 16:06:56 +0200

    Finished local version

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-import requests
-import json
+import requests, json, turtle
 
-response = requests.get('http://api.open-notify.org/iss-now.json')
+def move(length, width):
+    global iss
 
-if response.status_code == 200:
-    response_dict = json.loads(response.text)
-    location = response_dict['iss_position']
-    print(f"Current location:  {location['longitude']}, {location['latitude']}")
-else:
-    print('Request failed')
+    iss.penup()
+    iss.goto(length, width)
+    iss.pendown()
+
+def setup_display(display):
+    global iss
+    
+    display= turtle.Screen()
+    display.setup(1000,500)
+    display.bgpic('earth.gif')
+    display.setworldcoordinates(-180, -90, 180, 90)
+
+    iss = turtle.Turtle()
+    iss.shape('circle')
+    iss.color('black')
+
+def track_iss():
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+    if response.status_code == 200:
+        response_dict = json.loads(response.text)
+        location = response_dict['iss_position']
+        move(float(location['latitude']), float(location['longitude']))
+    else:
+        print('Request failed')
+    widget = turtle.getcanvas()
+    widget.after(1000, track_iss)
+
+global iss
+setup_display(turtle.Screen)
+track_iss()
+turtle.mainloop()
