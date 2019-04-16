8fcdc970e5c45194defb979eb338fb5bb8aed798
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-04-16 20:13:44 +0200

    Rewrote app for class

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
-import requests, json, turtle
+import requests
+import json
+import turtle
 
-def move(length, width):
-    global iss
 
-    iss.penup()
-    iss.goto(length, width)
-    iss.pendown()
+class IssTracker:
 
-def setup_display(display):
-    global iss
-    
-    display= turtle.Screen()
-    display.setup(1000,500)
-    display.bgpic('earth.gif')
-    display.setworldcoordinates(-180, -90, 180, 90)
+    def __init__(self):
+        self.iss = turtle.Turtle()
+        self.iss.shape('circle')
+        self.iss.color('black')
 
-    iss = turtle.Turtle()
-    iss.shape('circle')
-    iss.color('black')
+    def move(self, length, width):
+        self.iss.penup()
+        self.iss.goto(length, width)
+        self.iss.pendown()
 
-def track_iss():
-    response = requests.get('http://api.open-notify.org/iss-now.json')
-    if response.status_code == 200:
-        response_dict = json.loads(response.text)
-        location = response_dict['iss_position']
-        move(float(location['latitude']), float(location['longitude']))
-    else:
-        print('Request failed')
-    widget = turtle.getcanvas()
-    widget.after(1000, track_iss)
+    def setup_display(self, display):
+        display = turtle.Screen()
+        display.setup(1000, 500)
+        display.bgpic('earth.gif')
+        display.setworldcoordinates(-180, -90, 180, 90)
 
-global iss
-setup_display(turtle.Screen)
-track_iss()
+    def track_iss(self):
+        response = requests.get('http://api.open-notify.org/iss-now.json')
+        if response.status_code == 200:
+            response_dict = json.loads(response.text)
+            location = response_dict['iss_position']
+            self.move(float(location['latitude']), float(location['longitude']))
+        else:
+            print('Request failed')
+        widget = turtle.getcanvas()
+        widget.after(1000, self.track_iss)
+
+
+issTracker = IssTracker()
+issTracker.setup_display(turtle.Screen())
+issTracker.track_iss()
 turtle.mainloop()
